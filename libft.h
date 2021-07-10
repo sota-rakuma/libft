e7a0efcebf657406fb037782c1b2a4f40e026a34
srakuma <robben2159@gmail.com>
2021-07-10 21:04:50 +0900

    fix header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: srakuma <srakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 19:47:31 by srakuma           #+#    #+#             */
-/*   Updated: 2021/06/28 19:33:35 by srakuma          ###   ########.fr       */
+/*   Updated: 2021/07/08 22:00:12 by srakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@
 # include <limits.h>
 # include <unistd.h>
 
-typedef struct s_bcl
+typedef struct s_list
 {
-	int				value;
-	int				nil;
-	struct s_bcl	*prev;
-	struct s_bcl	*next;
-}				t_bcl;
+	void			*content;
+	struct s_list	*next;
+}				t_list;
 
 int		ft_atoi(const char *str);
 int		ft_isalnum(int c);
@@ -52,19 +50,19 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	ft_lstdelone(t_bcl *lst, void (*del)(int));
-t_bcl	*ft_lstmap(t_bcl *lst, int (*f)(int), void (*del)(int));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int nb, int fd);
 void	ft_putendl_fd(char *s, int fd);
 char	*ft_strdup(const char *s1);
-t_bcl	*ft_lstnew(int content);
-void	ft_lstadd_front(t_bcl **lst, t_bcl *new);
-int		ft_lstsize(t_bcl *first, t_bcl *last);
-t_bcl	*ft_lstlast(t_bcl *lst);
-void	ft_lstadd_back(t_bcl **lst, t_bcl *new);
-void	ft_lstiter(t_bcl *lst, void (*f)(int));
-void	ft_lstclear(t_bcl **lst, void (*del)(int));
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
