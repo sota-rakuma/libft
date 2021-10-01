d1b3401376d54670be7fece92966c02bec408f5a
srakuma <robben2159@gmail.com>
2021-10-01 22:15:30 +0900

    fix strncpy's type of arg

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,13 +6,13 @@
 /*   By: srakuma <srakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 23:14:42 by srakuma           #+#    #+#             */
-/*   Updated: 2021/10/01 22:13:36 by srakuma          ###   ########.fr       */
+/*   Updated: 2021/10/01 22:15:15 by srakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dest, char *src, size_t n)
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
 
