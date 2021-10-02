1b67dd1e6d1bb5a01cad1d00ba45c886db5c7cdd
srakuma <robben2159@gmail.com>
2021-10-02 19:08:12 +0900

    refine strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: srakuma <srakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 16:10:54 by srakuma           #+#    #+#             */
-/*   Updated: 2021/04/28 00:05:00 by srakuma          ###   ########.fr       */
+/*   Updated: 2021/10/02 19:07:48 by srakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int		i;
 	char	ch;
 
-	i = ft_strlen(s) + 1;
 	ch = (char)c;
-	while (i-- > 0)
+	if (c == '\0')
+		return ((char *)(s + ft_strlen(s)));
+	while (*s)
 	{
 		if (*s == ch)
 			return ((char *)s);
